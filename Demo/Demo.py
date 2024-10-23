4fef16138dfe9742cfaf9cf1ae45070c88672d48
CIF3 <woshiquanmouren@163.com>
2024-10-23 19:52:53 +0800

    添加demo

--- Demo/Demo.py
+++ Demo/Demo.py
@@ -1,15 +1,16 @@
 from RainbowColorizer import hue_to_rgb,RC
 
-
 logo = r"""  _____       _       _                      _____      _            _              
- |  __ \     (_)     | |                    / ____|    | |          (_)             
- | |__) |__ _ _ _ __ | |__   _____      __ | |     ___ | | ___  _ __ _ _______ _ __ 
- |  _  // _` | | '_ \| '_ \ / _ \ \ /\ / / | |    / _ \| |/ _ \| '__| |_  / _ | '__|
- | | \ | (_| | | | | | |_) | (_) \ V  V /  | |___| (_) | | (_) | |  | |/ |  __| |   
- |_|  \_\__,_|_|_| |_|_.__/ \___/ \_/\_/    \_____\___/|_|\___/|_|  |_/___\___|_|"""
+|  __ \     (_)     | |                    / ____|    | |          (_)             
+| |__) |__ _ _ _ __ | |__   _____      __ | |     ___ | | ___  _ __ _ _______ _ __ 
+|  _  // _` | | '_ \| '_ \ / _ \ \ /\ / / | |    / _ \| |/ _ \| '__| |_  / _ | '__|
+| | \ | (_| | | | | | |_) | (_) \ V  V /  | |___| (_) | | (_) | |  | |/ |  __| |   
+|_|  \_\__,_|_|_| |_|_.__/ \___/ \_/\_/    \_____\___/|_|\___/|_|  |_/___\___|_|"""
 print(RC.RainbowColorizer(logo))
 print(RC.RainbowColorizer("Welcome to Rainbow Colorizer"))
-RC.separator("/*-+","title")
+RC.separator("/","Better looking dividing lines")
 print(RC.RainbowColorizer("Use ") + "\033[31m[red]ANSI\033[0m" + RC.RainbowColorizer(" escape sequences more easily"))
-text = """############\n############\n############"""
+text = """##################\nCooler  four-color \ngradient rendering 
+function\n##################"""
 print(RC.colors4(text))
+print(RC.color("[bg(255,192,203)][red]Simpler background color dyeing[r]"))
