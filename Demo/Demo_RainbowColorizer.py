5bf05f13900d1305870ac14992a9d2c066ff2dae
CIF3 <woshiquanmouren@163.com>
2024-10-26 07:08:14 +0800

    修复bug更新Demo

--- Demo/Demo_RainbowColorizer.py
+++ Demo/Demo_RainbowColorizer.py
@@ -7,10 +7,10 @@ logo = r"""  _____       _       _                      _____      _            
  |  _  // _` | | '_ \| '_ \ / _ \ \ /\ / / | |    / _ \| |/ _ \| '__| |_  / _ | '__|
  | | \ | (_| | | | | | |_) | (_) \ V  V /  | |___| (_) | | (_) | |  | |/ |  __| |   
  |_|  \_\__,_|_|_| |_|_.__/ \___/ \_/\_/    \_____\___/|_|\___/|_|  |_/___\___|_|"""
-print(RC.RainbowColorizer(logo))
 print(RC.RainbowColorizer("Welcome to Rainbow Colorizer"))
+print(RC.RainbowColorizer(logo))
 print(RC.RainbowColorizer("Easier custom transition colors",(14,190,255),(255,66,179)))  # 更简单的自定义过度颜色
-print("\x1b[A\x1b[A\x1b[A\x1b[A\r\x1b[A\x1b[A\x1b[A\x1b[A\x1b[A\r\x1b[?25l")
+print("\x1b[A\x1b[A\x1b[A\r\x1b[A\x1b[A\x1b[A\x1b[A\x1b[A\r\x1b[?25l")
 for i in range(0, 360, 5):
     r,g,b = hue_to_rgb(i)
     r1,g1,b1 = hue_to_rgb(i+80)
