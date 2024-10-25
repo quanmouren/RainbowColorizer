019c7ef40a814de7fd42bda5a944c01ef678c7a9
CIF3 <woshiquanmouren@163.com>
2024-10-26 06:49:50 +0800

    更新Demo

--- Demo/Demo_RainbowColorizer.py
+++ Demo/Demo_RainbowColorizer.py
@@ -10,10 +10,10 @@ logo = r"""  _____       _       _                      _____      _            
 print(RC.RainbowColorizer(logo))
 print(RC.RainbowColorizer("Welcome to Rainbow Colorizer"))
 print(RC.RainbowColorizer("Easier custom transition colors",(14,190,255),(255,66,179)))  # 更简单的自定义过度颜色
-for i in range(3601):
+print("\x1b[A\x1b[A\x1b[A\x1b[A\r\x1b[A\x1b[A\x1b[A\x1b[A\x1b[A\r\x1b[?25l")
+for i in range(0, 360, 5):
     r,g,b = hue_to_rgb(i)
     r1,g1,b1 = hue_to_rgb(i+80)
-    print(RC.RainbowColorizer("Rainbow Colorizer",(r,g,b),(r1,g1,b1)),end='\r')
-
-    time.sleep(0.01)
-print()
+    print(RC.RainbowColorizer(logo,(r,g,b),(r1,g1,b1)),end='\x1b[A\x1b[A\x1b[A\x1b[A\x1b[A\r\x1b[?25l')
+    time.sleep(0.05)
+print("\n\n\n\n\n\n\n\x1b[?25h")
